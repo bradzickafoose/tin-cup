deb07aa8987951d83fb0a922a484b9b739553c66
Brad <brad@viziworx.com>
2020-09-21 14:29:52 -0400

    NEW: adds news + events page with base configuration including page title

---
+++ pages/news.js
@@ -0,0 +1,9 @@
+import Layout from 'components/layout/Layout';
+
+export default function NewsPage() {
+  return (
+    <Layout title="News + Events">
+      <h1>News + Events</h1>
+    </Layout>
+  );
+}
