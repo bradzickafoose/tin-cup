1a9c2b38bcedf19dd14adffd69afcdcb93cdf612
Brad <brad@viziworx.com>
2020-09-21 14:14:32 -0400

    📦 NEW: adds base configuration for custom 404

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import Layout from 'components/layout/Layout';
+
+export default function Custom404Page() {
+  return (
+    <Layout>
+      <h1>OOPS.</h1>
+      <p>Sorry, the page you are looking for isn't here.</p>
+      <Link href="/">
+        <a>Go back to the beginning?</a>
+      </Link>
+    </Layout>
+  );
+}
