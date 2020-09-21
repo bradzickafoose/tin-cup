7b2d966ee94d6c9e5d33d7a24456a620abce5dd1
Brad <brad@viziworx.com>
2020-09-21 15:31:41 -0400

    📦 NEW: adds events page with base configuration including page title and .map of post previews

---
+++ pages/events/index.js
@@ -0,0 +1,18 @@
+import Layout from 'components/layout/layout';
+import PostPreview from 'components/post-preview';
+
+export default function EventsPage() {
+  return (
+    <Layout title="News + Events">
+      <h1>News + Events</h1>
+      {posts.map(post => (
+        <PostPreview
+          key={post.slug}
+          title={post.title}
+          coverImage={post.coverImage}
+          slug={post.slug}
+        />
+      ))}
+    </Layout>
+  );
+}
--- pages/news.js
+++
@@ -1,9 +0,0 @@
-import Layout from 'components/layout/Layout';
-
-export default function NewsPage() {
-  return (
-    <Layout title="News + Events">
-      <h1>News + Events</h1>
-    </Layout>
-  );
-}
